1bef08cbdece78428e14df75181d7cc33dde793f
Warpointpro <joniii0550@gmail.com>
2025-08-02 12:59:29 -0300

    cambios

--- Blog_informatorio/settings.py
+++ Blog_informatorio/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.posts'
+    'apps.posts',
 ]
 
 MIDDLEWARE = [
