628168e7d9f9bf68dfe2405f257532b9f2b23dcd
Warpointpro <joniii0550@gmail.com>
2025-08-02 02:49:13 -0300

    asd

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
